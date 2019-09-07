43d42a764e3243e629425244570ee824f0a5ed66
diankind <diankind@gmail.com>
2019-09-07 19:12:12 +0300

    сделать functional_tests приложением, использовать LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
-import unittest
+from selenium.common.exceptions import WebDriverException
 
-class NewVisitorTest(unittest.TestCase):
+import time
+from django.test import LiveServerTestCase 
+
+MAX_WAIT = 10
+
+class NewVisitorTest(LiveServerTestCase):
     '''тест нового посетителя'''
     def setUp(self):
         '''установка'''
@@ -11,12 +15,27 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         '''демонтаж'''
         self.browser.quit()
+        
+    def wait_for_row_in_list_table(self, row_text):
+        '''ожидать строку в таблице списка'''
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)  
+ 
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получить его позже'''
         # Эдит слышала про крутое новое онлайн-приложение со
         # списком неотложных дел. Она решает оценить его
         # домашнюю страницу
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         # Она видит, что заголовок и шапка страницы говорят о
         # списках неотложных дел
@@ -38,18 +57,23 @@ class NewVisitorTest(unittest.TestCase):
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появился в таблице"
-        )
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья') 
         # Текствое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # Страница снова обновляется и теперь показывает оба элемента ее списка
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
         
+        # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
+        # сайт сгенерировал для нее уникальный URL-адрес – об этом
+        # выводится небольшой текст с пояснениями.
+
         self.fail('Закончить тест!')
-        # Страница снова обновляется и теперь показывает оба элемента
-        # ее списка
+        
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
