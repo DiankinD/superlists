b524f6d7ba88ad482644ba21cef9408d71991334
diankind <diankind@gmail.com>
2019-09-15 18:17:10 +0300

    изменили модель, добавили списки как класс

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class List(models.Model):
+    '''чей-то список'''
+    pass
+
 class Item(models.Model):
     '''элемент списка'''
-    text = models.TextField(default='') 
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
     pass
+
--- lists/views.py
+++ lists/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
-from lists.models import Item
+from lists.models import Item, List 
 
 # Create your views here.
 def home_page(request):
     '''домашняя страница'''
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/unique-list/')
     return render(request, 'home.html')
 
 def view_list(request):
     '''представление списка'''
     items = Item.objects.all()
-    return render(request, 'list.html', {'items': items}) 
+    return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    '''новый список'''
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/unique-list/')
