60182669ef1f61797a79a483f7a5a91d2a3c887e
diankind <diankind@gmail.com>
2019-09-19 20:39:30 +0300

    сделали отдельные списки

--- lists/urls.py
+++ lists/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
-from lists import views as list_views 
+from django.urls import path, re_path
+from lists import views
 
 
-urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('', list_views.home_page, name='home'),
-    path('lists/', include('lists.urls')),
-     
+urlpatterns = [    
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
 ]
