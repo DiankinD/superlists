fafa6eee0ee2aec7b5a93c072ecf2a762593674b
diankind <diankind@gmail.com>
2019-09-22 12:29:04 +0300

    Использован Bootstrap для улучшения макета

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,11 +5,11 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
 import time
-from django.test import LiveServerTestCase 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase 
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     '''тест нового посетителя'''
     def setUp(self):
         '''установка'''
@@ -123,6 +123,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
         # Она замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
